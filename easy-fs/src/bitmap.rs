3c331a5ba62f7bb938a4fedfcc3c4f3c0656ade3
traversalnat <1914619958@qq.com>
2022-07-27 15:44:48 +0800

    fmt of easy-fs

--- easy-fs/src/bitmap.rs
+++ easy-fs/src/bitmap.rs
@@ -1,9 +1,5 @@
+use super::{get_block_cache, BlockDevice, BLOCK_SZ};
 use alloc::sync::Arc;
-use super::{
-    BlockDevice,
-    BLOCK_SZ,
-    get_block_cache,
-};
 
 /// A bitmap block
 type BitmapBlock = [u64; 64];
@@ -38,14 +34,15 @@ impl Bitmap {
             let pos = get_block_cache(
                 block_id + self.start_block_id as usize,
                 Arc::clone(block_device),
-            ).lock().modify(0, |bitmap_block: &mut BitmapBlock| {
+            )
+            .lock()
+            .modify(0, |bitmap_block: &mut BitmapBlock| {
                 if let Some((bits64_pos, inner_pos)) = bitmap_block
                     .iter()
                     .enumerate()
                     .find(|(_, bits64)| **bits64 != u64::MAX)
-                    .map(|(bits64_pos, bits64)| {
-                        (bits64_pos, bits64.trailing_ones() as usize)
-                    }) {
+                    .map(|(bits64_pos, bits64)| (bits64_pos, bits64.trailing_ones() as usize))
+                {
                     // modify cache
                     bitmap_block[bits64_pos] |= 1u64 << inner_pos;
                     Some(block_id * BLOCK_BITS + bits64_pos * 64 + inner_pos as usize)
@@ -62,13 +59,12 @@ impl Bitmap {
     /// Deallocate a block
     pub fn dealloc(&self, block_device: &Arc<dyn BlockDevice>, bit: usize) {
         let (block_pos, bits64_pos, inner_pos) = decomposition(bit);
-        get_block_cache(
-            block_pos + self.start_block_id,
-            Arc::clone(block_device)
-        ).lock().modify(0, |bitmap_block: &mut BitmapBlock| {
-            assert!(bitmap_block[bits64_pos] & (1u64 << inner_pos) > 0);
-            bitmap_block[bits64_pos] -= 1u64 << inner_pos;
-        });
+        get_block_cache(block_pos + self.start_block_id, Arc::clone(block_device))
+            .lock()
+            .modify(0, |bitmap_block: &mut BitmapBlock| {
+                assert!(bitmap_block[bits64_pos] & (1u64 << inner_pos) > 0);
+                bitmap_block[bits64_pos] -= 1u64 << inner_pos;
+            });
     }
     /// Get the max number of allocatable blocks
     pub fn maximum(&self) -> usize {
--- easy-fs/src/block_dev.rs
+++ easy-fs/src/block_dev.rs
@@ -2,7 +2,7 @@ use core::any::Any;
 
 /// Trait for block devices
 /// which reads and writes data in the unit of blocks
-pub trait BlockDevice : Send + Sync + Any {
+pub trait BlockDevice: Send + Sync + Any {
     fn read_block(&self, block_id: usize, buf: &mut [u8]);
     fn write_block(&self, block_id: usize, buf: &[u8]);
 }
